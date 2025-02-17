72fcbae332f7b42a9ee66089ab3de764ae9b7cb9
mzwoelfer <>
2025-02-17 15:03:28 +0100

    Add Fedora41

--- DistroInfo.cs
+++ DistroInfo.cs
@@ -146,6 +146,16 @@ public class DistroInfo
     },
     new()
     {
+      Name = "Fedora41",
+      Family = "Fedora",
+      ImageName = "Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2",
+      Url = "https://download.fedoraproject.org/pub/fedora/linux/releases/41/Cloud/x86_64/images/",
+      Aliases = Array.Empty<string>(),
+      ChecksumFile = "Fedora-Cloud-41-1.4-x86_64-CHECKSUM",
+      ChecksumType = "sha256"
+    },
+    new()
+    {
       Name = "CentOS7",
       Family = "RHEL",
       ImageName = "CentOS-7-x86_64-GenericCloud.qcow2",
